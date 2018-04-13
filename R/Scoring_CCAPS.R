d19560754d2de3bca305e4b479bef49f412e1a04
“rbjanis” <rbjanis@gmail.com>
2018-04-12 21:59:51 -0400

    Adding documentation

--- R/Scoring_CCAPS.R
+++ R/Scoring_CCAPS.R
@@ -1,3 +1,11 @@
 #' A package to score the CCAPS subscales from CCAPS items
 #'
+#' @description
+#' @param
+#' @note \code{Scoring_CCAPS} calls on CCAPS items by name, so they must be named properly in
+#' \code{dat}. Variable naming convention is\code{CCAPS_01}... \code{CCAPS_70}.
+#' If \code{dat} does not contain the proper CCAPS variable names
+#' (or the naming convention was changed in the new year's data), the
+#' function will return an error.
+#' @return A data frame with all the original data in \code{dat}, and several additional columns: ...
 #'
