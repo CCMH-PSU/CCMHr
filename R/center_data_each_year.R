88f47e443b2b7b2d1a1cb5ddabf5fcd625501d4f
Rebecca <rbjanis@gmail.com>
2018-08-21 16:49:54 -0400

    Updating to stop if missing Data_year data

--- R/center_data_each_year.R
+++ R/center_data_each_year.R
@@ -12,12 +12,16 @@ center_data_each_year <- function(data) {
   if(!"Data_year" %in% names(data) | !"CenterID" %in% names(data)) {
     print("Data_year or CenterID columns missing.")
   } else{
-    years <- length(unique(data$Data_year))
-    group_by(data, Data_year, CenterID) %>%
-      summarize(n()) %>%
-      group_by(CenterID) %>%
-      summarize(N = n()) %>%
-      filter(N == years) %>%
-      semi_join(data, ., by = "CenterID")
+    if(sum(is.na(data$Data_year)) >0) {
+      print("Data_year variable contains missing data.")
+    } else{
+      years <- length(unique(data$Data_year))
+      group_by(data, Data_year, CenterID) %>%
+        summarize(n()) %>%
+        group_by(CenterID) %>%
+        summarize(N = n()) %>%
+        filter(N == years) %>%
+        semi_join(data, ., by = "CenterID")
+    }
   }
 }
