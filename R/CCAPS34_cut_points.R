2bbfbe382c2f57fa452a59e99c503a964a850129
Rebecca <rbjanis@gmail.com>
2018-12-07 14:32:43 -0500

    CCAPS34 low cut point function

---
+++ R/CCAPS34_cut_points.R
@@ -0,0 +1,29 @@
+#' A function to create CCAPS-34 cut points
+#'
+#' @description Creates binary variables for CCAPS-34 subscales indicating whether the score is above or below the low and high cut respectively, using the 2018 cut points.
+#' @param \code{dat} A data file containing CCAPS-34 subscales
+#' @note If \code{dat} does not contain the proper CCAPS subscale names, it will return an error.
+#' @return A data frame with all the original data in \code{dat}, and several additional columns: \code{Has_CCAPS},
+
+ccaps34_lowcut <- function(data){
+  if(all(c("Depression34", "Anxiety34", "Social_Anxiety34", "Academics34", "Eating34", "Hostility34", "Alcohol34", "DI") %in% colnames(data))) {
+    data %>%
+      mutate(depression_low_cut34 = ifelse(round(Depression34, 2) >= 1, 1, 0),
+             depression_hi_cut34 = ifelse(round(Depression34, 2) >= 1.83, 1, 0),
+             anxiety_low_cut34 = ifelse(round(Anxiety34, 2) >= 1.3, 1, 0),
+             anxiety_hi_cut34 = ifelse(round(Anxiety34,2) >= 2.1, 1, 0),
+             social_anxiety_low_cut34 = ifelse(round(Social_Anxiety34, 2) >= 1.65, 1, 0),
+             social_anxiety_hi_cut34 = ifelse(round(Social_Anxiety34,2) >= 2.5, 1, 0),
+             academics_low_cut34 = ifelse(round(Academics34,2) >= 1.45, 1, 0),
+             academics_hi_cut34 = ifelse(round(Academics34,2) >= 2.50, 1, 0),
+             eating_low_cut34 = ifelse(round(Eating34,2) >= 1.07, 1, 0),
+             eating_hi_cut34 = ifelse(round(Eating34,2) >= 1.50, 1, 0),
+             hostility_low_cut34 = ifelse(round(Hostility34,2) >= .74, 1, 0),
+             hostility_hi_cut34 = ifelse(round(Hostility34,2) >= 1.17, 1, 0),
+             alcohol_low_cut34 = ifelse(round(Alcohol34,2) >= .64, 1, 0),
+             alcohol_hi_cut34 = ifelse(round(Alcohol34,2) >= 1.10, 1, 0),
+             DI_low_cut = ifelse(round(DI,2) >= 1.21, 1, 0),
+             DI_hi_cut = ifelse(round(DI,2) >= 2.25, 1, 0)
+      )
+  } else ("CCAPS variables not correctly named")
+}
