86a65f329464e0954c98e0be671abf53df74484e
Rebecca <rbjanis@gmail.com>
2019-01-28 17:29:15 -0500

    Freqtab

---
+++ R/freqtab.R
@@ -0,0 +1,18 @@
+#' Frequency table
+#'
+#' @param x A vector
+#'
+#' @return A frequency table with percentages and cumulative percentages.
+#' @export
+#'
+#' @example
+#' df <- data.frame(y = c(1,1,1,2,2,3))
+#' freqtab(df$y)
+#'
+freqtab <- function(x) {
+  sym <- deparse(substitute(x))
+  descr::freq(ordered(x), plot = F) %>%
+    as.data.frame() %>%
+    tibble::rownames_to_column(var = sym)
+}
+
