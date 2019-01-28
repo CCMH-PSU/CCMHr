63009f4d3a3c0bcb6cef93fde1e354b30cda20ee
Rebecca <rbjanis@gmail.com>
2019-01-28 17:28:49 -0500

    Last present

---
+++ R/last_present.R
@@ -0,0 +1,15 @@
+#' Extract the last non-NA value
+#'
+#' @param x A vector
+#' @param order_by Another vector to sort `x` by
+#'
+#' @return The last non-NA value in `x`.
+#' @export
+#'
+#' @examples
+#'last_present(x = c(NA, 1, 2, NA), order_by = c(1, 2, 3, 4))
+#'last_present(x = c(NA, 1, 2, NA), order_by = c(4, 3, 2, 1))
+#'
+last_present <- function(x, order_by) {
+  na.omit(x[order(order_by, decreasing = T)])[1L]
+}
