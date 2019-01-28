0403a623b8bf5bc58df8f21275620d1d3a0285fd
Rebecca <rbjanis@gmail.com>
2019-01-28 17:29:31 -0500

    Fix varnames documentation

--- R/varnames.R
+++ R/varnames.R
@@ -1,4 +1,4 @@
-#' A Check for Variable Names
+#' Check variable Names
 #'
 #' @description
 #' This function is used to check the consistency of variable names for the new yearly data. Checks will be run against
@@ -7,12 +7,12 @@
 #'
 #' @param old_names A vector of strings indicating the old (or expected) variable names in the data set.
 #' The default is a vector of variable names coming from the 2016-2017 combined data, stored as an
-#' Rdata file in the package, called \code{CCMHvars}.
-#' @param dat The data frame containing the new year of (combined) data, with the variable names stored in \code{colnames(dat)}.
-#' @return A list containing \code{Is_same}, \code{Omitted}, and \code{Extra}. \code{Is_same} is boolean-valued,
-#' indicating whether or not the new variable names were consistent with the expected variable names. \code{Omitted}
-#' is a vector of strings corresponding to the variables from \code{old_names} not appearing in \code{dat}.
-#' \code{Extra} is a vector of strings corresponding to the variables in \code{dat} not appearing in \code{old_names}.
+#' Rdata file in the package, called `CCMHvars`.
+#' @param dat The data frame containing the new year of (combined) data, with the variable names stored in `colnames(dat)`.
+#' @return A list containing `Is_same`, `Omitted`, and `Extra`. `Is_same` is boolean-valued,
+#' indicating whether or not the new variable names were consistent with the expected variable names. `Omitted`
+#' is a vector of strings corresponding to the variables from `old_names` not appearing in `dat`.
+#' `Extra` is a vector of strings corresponding to the variables in \code{dat} not appearing in `old_names`.
 #' @examples
 #' old_names <- letters[1:3]
 #' dat <- as.data.frame(matrix(rnorm(12),3,4))
@@ -21,11 +21,8 @@
 #' @export
 
 
-
-
-varnames <- function(old_names=NULL,dat){
+varnames <- function(dat, old_names=NULL){
   if(is.null(old_names)){
-    data("CCMHvars")
     old_names <- CCMHvars
   }
   new_names <- colnames(dat)
