19c76fb4d2288daa9ff74eb461811b76e105e404
Rebecca <rbjanis@gmail.com>
2020-01-18 13:25:23 -0500

    Refactor first_present and last_present

--- R/first_present.R
+++ R/first_present.R
@@ -1,22 +1,30 @@
-#' Extract first non-NA values
+#' Extract first or last non-NA value from a vector
 #'
-#' @description Returns the first non-NA value of `x` when sorted by `order_by`.
+#' @description Returns the first or last non-NA value of `x` when sorted by `order_by`.
 #'
 #' @param x A vector
 #' @param order_by Another vector to sort `x` by.
+#' @param default A default value to use if the position does not exist in the input.
 #'
-#' @return The first non-NA value in `x`.
+#' @return The first or last non-NA value in `x`.
 #' @note If there are no non-NA values, NA is returned.
 #' @export
 #'
 #' @examples
-#'first_present(x = c(NA, 1, 2, NA), order_by = c(1, 2, 3, 4))
 #'first_present(x = c(NA, 1, 2, NA), order_by = c(4, 3, 2, 1))
+#'last_present(x = c(NA, 1, 2, NA), order_by = c(4, 3, 2, 1))
 #'
-first_present <- function(x, order_by = Date) {
-  stats::na.omit(x[order(order_by)])[1L]
+#'first_present(x = c(NA, 1, 2, NA), order_by = c(1, 2, 3, 4))
+#'last_present(x = c(NA, 1, 2, NA), order_by = c(1, 2, 3, 4))
+
+first_present <- function(x, order_by = NULL, default = NA) {
+  nth(x, 1L, order_by = order_by, default = default)
 }
 
-last_present <- function(x, order_by = Date) {
-  stats::na.omit(x[order(order_by, decreasing = T)])[1L]
+#' @export
+#' @rdname first_present
+
+last_present <- function(x, order_by = NULL, default = NA) {
+  nth(x, -1L, order_by = order_by, default = default)
 }
+
