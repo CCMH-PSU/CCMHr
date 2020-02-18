0f777cfd94546cd1c41fd508ff40fac9f2ca8ca0
Rebecca <rbjanis@gmail.com>
2020-02-17 19:52:45 -0500

    Simplify and add tests to check column classes

--- R/check_colum_classes.R
+++ R/check_colum_classes.R
@@ -11,11 +11,11 @@
 
 check_column_classes <- function(dataframe1 = survey, dataframe2 = appts){
   common_vars <- intersect(names(dataframe1), names(dataframe2))
-  dataframe2_classes <- data.frame(class_dataframe2 = sapply(dataframe2[,colnames(dataframe2) %in% common_vars], class), Column = common_vars)
-  dataframe1_classes <- data.frame(class_dataframe1 = sapply(dataframe1[,colnames(dataframe1) %in% common_vars], class), Column = common_vars)
-  dataframe1_classes$class_dataframe1 <- as.character(dataframe1_classes$class_dataframe1)
-  dataframe2_classes$class_dataframe2 <- as.character(dataframe2_classes$class_dataframe2)
-  df <- dplyr::full_join(dataframe2_classes, dataframe1_classes) %>%
+
+  dataframe2_classes <- tibble::tibble(class_dataframe2 = purrr::map_chr(dataframe2[,colnames(dataframe2) %in% common_vars], class), Column = common_vars)
+  dataframe1_classes <- tibble::tibble(class_dataframe1 = purrr::map_chr(dataframe1[,colnames(dataframe1) %in% common_vars], class), Column = common_vars)
+
+  dplyr::full_join(dataframe2_classes, dataframe1_classes) %>%
     dplyr::filter(class_dataframe2 != class_dataframe1) %>%
     dplyr::select(Column, class_dataframe1, class_dataframe2)
   names(df) <- c("Column", rlang::expr_text(substitute(dataframe1)), rlang::expr_text(substitute(dataframe2)))
---
+++ tests/testthat/test-check_colum_classes.R
@@ -0,0 +1,12 @@
+context("check_column_classes")
+
+library(CCMHr)
+library(testthat)
+
+test1 <- data.frame(a = c(1, 2, 3), b = c(1, 2, 3), c = ("string"))
+test2 <- data.frame(a = c(1, 2, 3), B = c(1, 2, 3), c = c(1, 2, 3))
+
+test_that("check column classes works", {
+  expect_equal(check_column_classes(test1, test2),
+               tibble::tibble(Column = c("c"), test1 = c("factor"), test2 = c("numeric")))
+})
