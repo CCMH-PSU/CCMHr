052ded4197e15a6f1b88f9550d942ae0a017e9f5
Rebecca <rbjanis@gmail.com>
2020-09-28 12:22:33 -0400

    Remove gmp dependency

--- R/find_cutpoint_boundaries.R
+++ R/find_cutpoint_boundaries.R
@@ -11,10 +11,10 @@ find_cutpoint_boundaries <- function(items, cutpoint) {
 # Find the possible scores closest to the cut point, above and below it
 # If there's a score == cut point, return that, as well as the ones above and below
   unique_scores <- function(items) {
-    subscale <- as.data.frame(arrangements::combinations(x = c(0,1,2,3,4), k = items, replace=TRUE))
+    subscale <- as.data.frame(gtools::combinations(5, items, v=c(0,1,2,3,4), set=F, repeats.allowed=T))
     subscale$Score <- round(apply(subscale, 1, mean), 2)
-    subscale <- dplyr::arrange(subscale, Score) %>%
-      dplyr::group_by(Score) %>%
+    subscale <- dplyr::arrange(subscale, .data$Score) %>%
+      dplyr::group_by(.data$Score) %>%
       dplyr::slice(1) %>%
       dplyr::mutate(Items = items)
     return(subscale)
@@ -23,17 +23,17 @@ find_cutpoint_boundaries <- function(items, cutpoint) {
   admins <- Filter(function(x) round(1-(x/items),2) < .33, seq(1, items, by = 1))
 
   purrr::map_df(admins, unique_scores) %>%
-      dplyr::arrange(desc(Items)) %>%
-      dplyr::group_by(Score) %>%
+      dplyr::arrange(plyr::desc(.data$Items)) %>%
+      dplyr::group_by(.data$Score) %>%
       dplyr::summarize_all(dplyr::first) %>%
       # dplyr::filter(Score == max(Score < cutpoint)|Score == max(Score > cutpoint))
       dplyr::filter(Score == max(Score[Score < cutpoint])|
                       Score == min(Score[Score >= cutpoint])|
                       Score == cutpoint) %>%
-    dplyr::mutate(result = case_when(Score < cutpoint ~ "Below",
+    dplyr::mutate(result = dplyr::case_when(Score < cutpoint ~ "Below",
                                        Score > cutpoint ~ "Above",
                                        Score == cutpoint ~ "Equal (above)")) %>%
-    dplyr::select(-Items) %>%
+    dplyr::select(-.data$Items) %>%
     dplyr::mutate(cutpoint = cutpoint) %>%
     dplyr::select(Score, cutpoint, result, dplyr::everything())
 }
