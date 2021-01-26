7f6b015eb6ccc2bb06330a47042cf49ff34df842
Rebecca <rbjanis@gmail.com>
2021-01-26 12:14:18 -0500

    Add CCMH caption function

--- R/plot_theme.R
+++ R/plot_theme.R
@@ -1,6 +1,12 @@
-#' CCMH Plot Theme
+#' CCMH Plot Theme and Attribution Caption
 #'
-#' @description Sets the font to Avenir and adds space between the axis titles and the plot.
+#' @description
+#'   `ccmh_theme` Sets the font to Avenir and adds space between the axis titles and the plot. \cr
+#'   `ccmh_caption` adds at attribution caption to the plot ("Source: The Center for Collegiate Mental Health")
+#'
+#' @param plot_title The font size of the plot title
+#' @param axis_title The font size of the axis title
+#' @param axis_text The font size of the axis labels
 #'
 #' @export
 #'
@@ -11,4 +17,11 @@ ccmh_theme <- function(plot_title = 20, axis_title = 16, axis_text = 14) {
                  plot.title = element_text(size = title_size),
                  axis.title = element_text(size = axis_title),
                  axis.text = element_text(size = axis_text))
+}
+
+#' @export
+#' @rdname ccmh_theme
+
+ccmh_caption <- function() {
+  ggplot2::labs(caption = "Source: The Center for Collegiate Mental Health")
 }
