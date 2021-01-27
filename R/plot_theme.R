70edf373d31f6d0eaea1a39c94a24cdafcec0c1e
Rebecca <rbjanis@gmail.com>
2021-01-27 11:02:27 -0500

    Fix arguments in ccmh_theme

--- R/plot_theme.R
+++ R/plot_theme.R
@@ -14,7 +14,7 @@ ccmh_theme <- function(plot_title = 20, axis_title = 16, axis_text = 14) {
   ggplot2::theme(text = ggplot2::element_text(family = "Avenir"),
                  axis.title.y = ggplot2::element_text(margin = ggplot2::margin(t = 0, r = 7, b = 0, l = 0)),
                  axis.title.x = ggplot2::element_text(margin = ggplot2::margin(t = 7, r = 0, b = 0, l = 0)),
-                 plot.title = element_text(size = title_size),
+                 plot.title = element_text(size = plot_title),
                  axis.title = element_text(size = axis_title),
                  axis.text = element_text(size = axis_text))
 }
