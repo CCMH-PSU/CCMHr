cec3e342d332946ea75e1e07da1ede15918a8bb7
Rebecca <rbjanis@gmail.com>
2021-02-01 22:43:18 -0500

    Add caption to plot theme

--- R/plot_theme.R
+++ R/plot_theme.R
@@ -7,17 +7,20 @@
 #' @param plot_title The font size of the plot title
 #' @param axis_title The font size of the axis title
 #' @param axis_text The font size of the axis labels
+#' @param caption_text The font size for the caption
 #'
 #' @export
 #'
 ccmh_theme <- function(plot_title = 20, axis_title = 16, axis_text = 14, caption_text = 12) {
+  ggplot2::theme_minimal() +
   ggplot2::theme(text = ggplot2::element_text(family = "Avenir"),
                  axis.title.y = ggplot2::element_text(margin = ggplot2::margin(t = 0, r = 7, b = 0, l = 0)),
                  axis.title.x = ggplot2::element_text(margin = ggplot2::margin(t = 7, r = 0, b = 0, l = 0)),
                  plot.title = element_text(size = plot_title),
                  axis.title = element_text(size = axis_title),
                  axis.text = element_text(size = axis_text),
-                 plot.caption = element_text(size = caption_text))
+                 plot.caption = element_text(size = caption_text)) +
+    ccmh_caption()
 }
 
 #' @export
