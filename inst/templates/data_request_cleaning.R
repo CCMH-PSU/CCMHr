48c041d16495d49d0a82bd87831313613ddd0771
Rebecca <rbjanis@gmail.com>
2021-04-07 08:54:00 -0400

    Remove therapist and center variables from data request cleaning

--- inst/templates/data_request_cleaning.R
+++ inst/templates/data_request_cleaning.R
@@ -12,7 +12,40 @@ data <- CCMHr::remove_free_response(data)
 data <- CCMHr::remove_empty(data)
 data <- select(data, -contains("AUDIT"))
 data <- select(data, -contains("Dx"))
-data <- select(data, -c(ClientID, TherapistAge:Theory_Systems, CcapsVer2012StartDate:CaseNoteID, Enrollment:CCAPSFrequencyX))
+data <- select(data,
+               -tidyselect::any_of(c("ClientID", "TherapistAge", "TherAge",
+                                   "Enrollment", "Public_Private", "Grading_Scale",
+                                   "Athletic_Division", "APA_Accredited_Training",
+                                   "IACS_Accredited", "Services_Career",
+                                   "Services_Disability", "Services_Drug_Alcohol",
+                                   "Services_EAP", "Services_Learning",
+                                   "Services_Health", "Services_Testing",
+                                   "Services_Other", "Psychiatric_Services",
+                                   "Session_Limit", "Session_Limit_Amount",
+                                   "ChargeFor_Intake", "ChargeFor_Individual",
+                                   "ChargeFor_Group",
+                                   "ChargeFor_PsychiatricEvaluation",
+                                   "ChargeFor_PsychiatricFollowup",
+                                   "ChargeFor_AssessmentPsychological",
+                                   "ChargeFor_AssessmentCareer",
+                                   "ChargeFor_AssessmentDisability",
+                                   "ChargeFor_Other","UserID", "UserAttendance",
+                                   "TherGender", "TherEthnicity", "Highest_Degree",
+                                   "Highest_Degree_Discipline",
+                                   "Highest_Degree_YearReceived",
+                                   "Highest_Degree_Licensed", "Year_Licensed",
+                                   "Position", "Theory_Analytic", "Theory_Behavioral",
+                                   "Theory_Cognitive", "Theory_Humanistic",
+                                   "Theory_Systems", "EnrollmentCount", "CCAPSFrequency",
+                                   "CCAPSFrequencyX", "CcapsVer2012StartDate",
+                                   "CcapsVer2015StartDate", "CenterWideConsent",
+                                   "ClientID", "UserID.1", "Year",
+                                   "Month", "Day", "OnMultipleSchedules",
+                                   "DaysInTreatment", "TreatmentCourse",
+                                   "UniquePreviousTherapist", "CaseNoteID",
+                                   "HasSurvey", "HasCCAPS", "HasCCAPS34",
+                                   "HasCCAPS62", "HasSDS", "HasCLICC",
+                                   "HasCLOSURE", "UniqueScheduledTherapist")))
 
 # Removing duplicate appointments
 data <- CCMHr::delete_duplicate_appointments(data)
