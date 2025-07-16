aee9b9d322fd1612d34a5e23e04c0327cc66278c
Tyler Dallas White <tylerwhite0120@gmail.com>
2025-07-16 15:36:39 -0400

    Updated documentation, standardized spacing/tabs, and added code notes when applicable. Also, fixed an error related to the return function and the RHS call of a pipe.

--- R/center_data_each_year.R
+++ R/center_data_each_year.R
@@ -1,22 +1,44 @@
-#' Selecting only centers who contributed data for all years.
+#' Filter for centers that contributed data for all years within a data frame.
 #'
-#' @param data A data frame with multiple years of CCMH data.
+#' @description A center may contribute data for research each year, some years, or a single year. For some analyses, it could be critical to include only centers that contribute data annually within a data frame. This function excludes centers that did not contribute research data every year. A valid "Data_year" and "CcmhID" variables must exist in the data frame to run the function.
 #'
-#' @return A data frame with only centers contributing data for all data years included in the original data set.
+#' @param data A data frame with multiple years of data.
+#'
+#' @return A data frame with only centers contributing data for all years included in the original data frame.
+#'
 #' @export
 #'
-#'
-center_data_each_year <- function(data) {
-  if(!"Data_year" %in% names(data) | !"CcmhID" %in% names(data)) {
+
+center_data_each_year <- function(data){
+
+  # Check for Data_year and CcmhID variables
+  if(!"Data_year" %in% names(data) |
+     !"CcmhID" %in% names(data)){
+
+    # Message
     stop("Data_year or CcmhID columns missing.")
+
   } else{
-    if(sum(is.na(data$Data_year) | is.na(data$CcmhID)) >0) {
+
+    # Check for missing data in Data_year or CcmhID variables
+    if(sum(is.na(data$Data_year) |
+           is.na(data$CcmhID)) > 0) {
+
       stop("Data_year variable contains missing data.")
+
     } else{
-      dplyr::group_by(data, .data$CcmhID) %>%
-        dplyr::filter(dplyr::n_distinct(data$Data_year) == length(unique(.data$Data_year))) %>%
-        dplyr::ungroup() %>%
-        return()
+
+      # Filter for centers with data for all years
+      data <- data |>
+        dplyr::group_by(CcmhID) |>
+        dplyr::filter(dplyr::n_distinct(Data_year) == length(unique(Data_year))) |>
+        dplyr::ungroup()
+
+      # Return data frame
+      return(data)
+
     }
+
   }
+
 }
