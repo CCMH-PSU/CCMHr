a73c26d1a14ba6fc1811106af36305275a036d56
Tyler Dallas White <tylerwhite0120@gmail.com>
2025-07-16 16:47:14 -0400

    Updated documentation, standardized spacing/tabs, and added code notes when applicable.

--- R/delete_duplicate_appointments.R
+++ R/delete_duplicate_appointments.R
@@ -1,41 +1,67 @@
-#' Delete Duplicate Appointments
+#' Delete duplicate appointments.
 #'
-#' @param data A data frame with surveys and appointments.
-#' @param client_identifier The column uniquely identifying each client. By default, `UniqueClientID`.
-#' @param appointment_identifier The column uniquely identifying each appointment. By default `AppointID`, but if this is not available, use `Date` instead.
+#' @description For reasons not discussed here, data on appointments may have duplicates (e.g., multiple rows with the same type of data). This function removes duplicate data from appointments.
 #'
-#' @return A data frame with only the first instance of each duplicated appointment.
+#' @param data A data frame containing rows related to appointments.
+#' @param client_identifier A quoted string to indicate the column uniquely identifying each client. By default, `"UniqueClientID"`.
+#' @param appointment_identifier A quoted string to indicate the column uniquely identifying each appointment. By default, `AppointID`. If `"AppointID"` is not available, use `"Date"` instead.
+#'
+#' @return A data frame with only the first instance of each appointment.
+#'
 #' @export
-#'
-delete_duplicate_appointments <- function(data, client_identifier = "UniqueClientID", appointment_identifier = "AppointID") {
 
-  if (!client_identifier %in% names(data)) {
+delete_duplicate_appointments <- function(data,
+                                          client_identifier = "UniqueClientID",
+                                          appointment_identifier = "AppointID") {
+
+  # Indicate if the client_identifier is present in the data
+  if (!client_identifier %in% names(data)){
+
     stop(glue::glue("client_identifier ({client_identifier}) not present in the data."))
+
+  } else{
+
   }
 
-  if(!appointment_identifier %in% names(data)) {
+  # Indicate if the appointment_identifier is present in the data
+  if(!appointment_identifier %in% names(data)){
+
     stop(glue::glue("appointment_identifier ({appointment_identifier}) not present in the data."))
+
+  } else{
+
   }
 
+  # Convert the appointment_identifier to a symbol
   appointment_identifier_temp <- rlang::sym(appointment_identifier)
 
-  appt <- dplyr::filter(data, !is.na(!!appointment_identifier_temp)) # creates a data frame of only appointments
+  # Creates a data frame of only appointments
+  appt <- dplyr::filter(data, !is.na(!!appointment_identifier_temp))
+
+  # Creates a data frame of only survey
   survey <- dplyr::filter(data, is.na(!!appointment_identifier_temp))
 
+  # Detect duplicates
   appt$duplicate <- duplicated(appt[c(client_identifier,appointment_identifier)])
 
-  appt <- dplyr::filter(appt, .data$duplicate == FALSE) %>%
+  # Remove duplicates
+  appt <- dplyr::filter(appt, .data$duplicate == FALSE) |>
     dplyr::select(-.data$duplicate)
 
+  # Recombine the data frames and arrange by client, date, or appointment
   if ("Date" %in% names(data)) {
-    data <- rbind(appt, survey) %>%
+
+    data <- rbind(appt, survey) |>
       dplyr::arrange(.data$UniqueClientID, .data$Date)
+
   } else {
-    data <- rbind(appt, survey) %>%
+
+    data <- rbind(appt, survey) |>
       dplyr::arrange(.data$UniqueClientID, !!appointment_identifier_temp)
+
   }
 
-  data
-
+  # Return data
+  return(data)
 
 }
