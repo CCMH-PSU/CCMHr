58e449794f498de8b10201c1dba5cb162f24f7d5
Tyler Dallas White <tylerwhite0120@gmail.com>
2025-07-16 14:22:46 -0400

    Updated documentation, standardized spacing/tabs, and added code notes when applicable. Also, made create_Data_year more robust.

--- R/Data_year.r
+++ R/Data_year.r
@@ -1,106 +1,115 @@
-#' Create Data_year variable
+#' Create Data_year variable.
 #'
-#' @param data  A data file.
+#' @description Creates the "Data_year" variable within a data set. The "Data_year" variable refers to the academic year in which the data was collected. To run this function, the following must be true: a) the "Data_year" variable must be absent from the data frame, b) the "Date" variable must exist in the data frame, c) the "Date" variable must be classed as a date, and d) the "Date" variable must be in the following format "%Y/%m/%d" (ex. "2025-01-20"). To date, the function creates the "Data_year" variable for a date frame with dates between "2010-07-01" and "2035-07-01".
 #'
-#' @return A data frame with a new variable Data_year.
+#' @param data  A data frame with a valid Date variable.
+#'
+#' @return A data frame with a new variable, Data_year.
+#'
 #' @export
 #'
-#' 
 
 create_Data_year <- function(data){
 
-    # Stop if Data_year already exists
-    if("Data_year" %in% colnames(data)){
-        stop("Data_year already exists")
+  # Stop if Data_year already exists
+  if("Data_year" %in% colnames(data)){
+
+    stop("Data_year already exists")
+
+  } else{
+
+  }
+
+  # Stop if Date does not exist
+  if(!"Date" %in% colnames(data)){
+
+    stop("Variable Date does not exist")
+
+  } else{
+
+  }
+
+  # Stop if Date is not classified as a date
+  if(!is.Date(data$Date)){
+
+    stop("Variable Date is not classified as a date")
+
+  } else{
+
+  }
+
+  # Stop if Date format is incorrect
+    # Function to check if date is in correct format
+    IsDate <- function(mydate,
+                       date.format = "%Y/%m/%d") {
+
+      tryCatch(!is.na(as.Date(mydate, date.format)),
+               error = function(err) {FALSE})
+
     }
 
-    # Stop if Date does not exist
-    if(!"Date" %in% colnames(data)){
-        stop("Variable Date does not exist")
-    }
+    # Check if Date is correctly formatted
+    check.date.list <- IsDate(data$Date)
 
-    # Stop if Date is not classified as a date
-    if(!is.Date(data$Date)){
-        stop("Variable Date is not classified as a date")
-    }
-
-    # Stop if Date format is incorrect
-        # Function to check if date is in correct format
-        IsDate <- function(mydate, date.format = "%Y/%m/%d") {
-            tryCatch(!is.na(as.Date(mydate, date.format)),  
-                    error = function(err) {FALSE})  
-        }
-
-        # Check if Date is in correct format
-        check.date.list <- IsDate(data$Date)
-
+    # Stop if date is not correctly formatted
     if(any(check.date.list == FALSE)){
+
         stop("Variable Date is not formatted correctly: %Y/%m/%d")
+
+    } else{
+
     }
 
-    # Create Data_year
-    data <- data %>%
-        dplyr::mutate(Data_year = case_when(Date >= "2035-07-01" ~ 2035,
-                                            Date >= "2034-07-01" ~ 2034,
-                                            Date >= "2033-07-01" ~ 2033,
-                                            Date >= "2032-07-01" ~ 2032,
-                                            Date >= "2031-07-01" ~ 2031,
-                                            Date >= "2030-07-01" ~ 2030,
-                                            Date >= "2029-07-01" ~ 2029,
-                                            Date >= "2028-07-01" ~ 2028,
-                                            Date >= "2027-07-01" ~ 2027,
-                                            Date >= "2026-07-01" ~ 2026,
-                                            Date >= "2025-07-01" ~ 2025,
-                                            Date >= "2024-07-01" ~ 2024,
-                                            Date >= "2023-07-01" ~ 2023,
-                                            Date >= "2022-07-01" ~ 2022,
-                                            Date >= "2021-07-01" ~ 2021,
-                                            Date >= "2020-07-01" ~ 2020,
-                                            Date >= "2019-07-01" ~ 2019,
-                                            Date >= "2018-07-01" ~ 2018,
-                                            Date >= "2017-07-01" ~ 2017,
-                                            Date >= "2016-07-01" ~ 2016,
-                                            Date >= "2015-07-01" ~ 2015,
-                                            Date >= "2014-07-01" ~ 2014,
-                                            Date >= "2013-07-01" ~ 2013,
-                                            Date >= "2012-07-01" ~ 2012,
-                                            Date >= "2011-07-01" ~ 2011,
-                                            Date >= "2010-07-01" ~ 2010))
+  # Create Data_year
+  data <- data |>
+    dplyr::mutate(Data_year = dplyr::if_else(month(Date) >= 7, year(Date), year(Date) - 1))
 
-    return(data)
+  # Return data
+  return(data)
 
 }
 
-#' Format Data_year variable
+#' Format Data_year variable.
 #'
-#' @param data  A data file.
+#' @description Creates different formats of the "Data_year" variable within a data frame. This function will create three new variables within a data frame. The variables include Data_year_long (ex. "2020-2021"), Data_year_formal (ex. "2020-21"), and Data_year_formalshort (ex. "20-21"). These formatted variables are often used at CCMH for axis labels within trend plots. To run this function, a valid "Data_year" variable must exist within the data frame.
 #'
-#' @return A data frame with new variables: Data_year_long, Data_year_formal, and Data_year_formalshort.
+#' @param data  A data frame with a valid Data_year variable.
+#'
+#' @return A data frame with new variables Data_year_long, Data_year_formal, and Data_year_formalshort.
+#'
 #' @export
 #'
 
 format_Data_year <- function(data){
 
-    # Stop if Data_year does not exist
-    if(!"Data_year" %in% colnames(data)){
-        stop("Variable Data_year does not exist")
-    }
+  # Stop if Data_year does not exist
+  if(!"Data_year" %in% colnames(data)){
 
-    # Stop if Data_year has more than less or more than 4 digits
-    if(any(nchar(data$Data_year) != 4)){
-        stop("Variable Data_year format is incorrect. Values within the Data_year vector should have 4 digits (for example, 2021)")
-    }
+    stop("Variable Data_year does not exist")
 
-    # Making Data_year as a numberic 
-    data$Data_year <- as.numeric(data$Data_year)
+  } else{
 
-    # Adding new Data_year variables
-    data <- data %>%
-        dplyr::mutate(Data_year_long = paste0(Data_year, "-", Data_year + 1),
-                      Data_year_formal = paste0(Data_year, "-", stringr::str_sub(Data_year + 1, start = -2)),
-                      Data_year_formalshort = paste0(stringr::str_sub(Data_year, start = -2), "-", stringr::str_sub(Data_year + 1, start = -2)))
+  }
 
-    # Return data
-    return(data)
+  # Stop if Data_year has more than less or more than 4 digits
+  if(any(nchar(data$Data_year) != 4)){
+
+    stop("Variable Data_year format is incorrect. Values within the Data_year vector should have 4 digits (for example, 2020)")
+
+  } else{
+
+  }
+
+  # Making Data_year as a numeric
+  data$Data_year <- as.numeric(data$Data_year)
+
+  # Adding new Data_year variables
+  data <- data |>
+    dplyr::mutate(Data_year_long = paste0(Data_year, "-", Data_year + 1),
+                  Data_year_formal = paste0(Data_year, "-", stringr::str_sub(Data_year + 1, start = -2)),
+                  Data_year_formalshort = paste0(stringr::str_sub(Data_year, start = -2), "-", stringr::str_sub(Data_year + 1, start = -2)))
+
+  # Return data
+  return(data)
 
 }
