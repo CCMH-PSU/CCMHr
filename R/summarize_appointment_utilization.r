902db172c8bb6f31ff72aa48b3362f5e6f653ae9
Tyler Dallas White <tylerwhite0120@gmail.com>
2025-08-05 10:44:12 -0400

    Updated documentation, standardized spacing/tabs, and added code notes when applicable.

--- R/summarize_appointment_utilization.r
+++ R/summarize_appointment_utilization.r
@@ -1,66 +1,85 @@
-#' Summarize appointment utilization
+#' Summarize appointment utilization.
 #'
-#' @description Clean and summarize individual and group therapy appointment utilization
-#' @param data A data file containing information on individual and group therapy appointments.
-#' @param by_year A logical statement to indicate if appointment utilization should be summarized by year instead of overall. If `TRUE`, the appointment utilization of each client will be summarized by year. If `FALSE`, the appointment utilization of each client will be summarized across years. Default = `FALSE`.
-#' @param recode_NA A logical statement to indicate if NA should be recoded to 0. If `TRUE`, NA will be recoded to 0. Default = `FALSE`.
-#' @return A data frame with cleaning appointment utilization data.
+#' @description This function cleans and summarizes data concerning individual and group therapy appointment utilization. The function returns a dataset containing summarized appointment utilization data of each client. The summarization process creates the following new variables: total.scheduled.appts (Total number of scheduled appointments), total.scheduled.ind.appts (Total number of scheduled individual appointments), total.scheduled.group.appts (Total number of scheduled group appointments), total.attended.appts (Total number of attended appointments), total.attended.ind.appts (Total number of attended individual appointments), and total.attended.group.appts (Total number of attended group appointments).
+#'
+#' @param data A data frame containing information on appointment data.
+#' @param by_year A logical argument to indicate whether individual/group therapy appointment utilization should be summarized by year instead of overall. If `TRUE`, the appointment utilization of each client will be summarized by year. If `FALSE`, the appointment utilization of each client will be summarized across all years. By default, `FALSE`.
+#' @param recode_NA A logical argument to indicate if NA should be recoded to 0. If `TRUE`, NA will be recoded to 0. By default, `FALSE`.
+#'
+#' @return A dataset containing details on clients' individual and group therapy appointment utilization. Variables in the data frame include UniqueClientID, CcmhID, Data_year, total.scheduled.appts, total.scheduled.ind.appts, total.scheduled.group.appts, total.attended.appts, total.attended.ind.appts, and total.attended.group.appts.
+#'
+#' @importFrom dplyr filter
+#' @importFrom dplyr select
+#' @importFrom dplyr mutate
+#' @importFrom dplyr summarise
+#' @importFrom dplyr across
+#' @importFrom dplyr n
+#' @importFrom dplyr starts_with
+#'
 #' @export
-#' 
 
-summarize_appointment_utilization <- function(data, 
-                                              by_year = FALSE, 
+summarize_appointment_utilization <- function(data,
+                                              by_year = FALSE,
                                               recode_NA = FALSE) {
 
-    # Check if specified columns are present in the data
-    if (!all(c("UniqueClientID", "CcmhID", 
-               "AppointID", "Data_year", 
-               "ClientAttendance", "AppointmentCategory", 
-               "Attended") %in% colnames(data))) {
-        stop("The data does not contain the required columns: UniqueClientID, CcmhID, AppointID, Data_year, ClientAttendance, AppointmentCategory, Attended.")
-    }
+  # Check if specified columns are present in the data
+  if(!all(c("UniqueClientID", "CcmhID",
+            "AppointID", "Data_year",
+            "ClientAttendance", "AppointmentCategory",
+            "Attended") %in% colnames(data))){
 
-    # Clean the data
-    data <- data %>%
-        dplyr::filter(Is_appointment == 1) %>% 
-        dplyr::select(UniqueClientID, CcmhID, 
-                      AppointID, Data_year, 
-                      ClientAttendance, AppointmentCategory, 
-                      Attended) %>%
-        CCMHr::delete_duplicate_appointments() %>% 
-        dplyr::filter(!ClientAttendance %in% c("Scheduled", "Counselor Cancelled", 
-                                                "Counselor Rescheduled", "Center Closed")) %>% 
-        dplyr::filter(AppointmentCategory %in% c(2,3,13,14,15)) %>% 
-        dplyr::mutate(appts_type = ifelse(AppointmentCategory %in% c(2,3), "Individual", "Group"))
-        
-    # By year
-    if(by_year == TRUE) {
-        data <- data %>%
-            dplyr::summarise(total.scheduled.appts = dplyr::n(),
-                             total.scheduled.ind.appts = length(which(appts_type == "Individual")),
-                             total.scheduled.group.appts = length(which(appts_type == "Group")),
-                             total.attended.appts = sum(Attended), 
-                             total.attended.ind.appts = length(which(appts_type == "Individual" & Attended == 1)),
-                             total.attended.group.appts = length(which(appts_type == "Group" & Attended == 1)),
-                             .by = c("UniqueClientID", "CcmhID", "Data_year"))
-    } else {
-        data <- data %>%
-            dplyr::summarise(total.scheduled.appts = dplyr::n(),
-                             total.scheduled.ind.appts = length(which(appts_type == "Individual")),
-                             total.scheduled.group.appts = length(which(appts_type == "Group")),
-                             total.attended.appts = sum(Attended), 
-                             total.attended.ind.appts = length(which(appts_type == "Individual" & Attended == 1)),
-                             total.attended.group.appts = length(which(appts_type == "Group" & Attended == 1)),
-                             .by = c("UniqueClientID", "CcmhID"))
-    }
+    stop("The data does not contain the required columns: UniqueClientID, CcmhID, AppointID, Data_year, ClientAttendance, AppointmentCategory, Attended.")
 
-    # Recode NA to 0
-    if(recode_NA == TRUE) {
-        data <- data %>%
-            dplyr::mutate(dplyr::across(dplyr::starts_with("total"), ~ifelse(.x == 0, NA, .x)))
-    } else {
-       # Skip
-    }
+  } else{
+
+  }
+
+  # Clean the data
+  data <- data |>
+    dplyr::filter(Is_appointment == 1) |>
+    dplyr::select(UniqueClientID, CcmhID,
+                  AppointID, Data_year,
+                  ClientAttendance, AppointmentCategory,
+                  Attended) |>
+    CCMHr::delete_duplicate_appointments() |>
+    dplyr::filter(!ClientAttendance %in% c("Scheduled", "Counselor Cancelled",
+                                           "Counselor Rescheduled", "Center Closed")) |>
+    dplyr::filter(AppointmentCategory %in% c(2,3,13,14,15)) |>
+    dplyr::mutate(appts_type = ifelse(AppointmentCategory %in% c(2,3), "Individual", "Group"))
+
+  # By year argument
+  if(by_year == TRUE){
+
+    data <- data |>
+      dplyr::summarise(total.scheduled.appts = dplyr::n(),
+                       total.scheduled.ind.appts = length(which(appts_type == "Individual")),
+                       total.scheduled.group.appts = length(which(appts_type == "Group")),
+                       total.attended.appts = sum(Attended),
+                       total.attended.ind.appts = length(which(appts_type == "Individual" & Attended == 1)),
+                       total.attended.group.appts = length(which(appts_type == "Group" & Attended == 1)),
+                       .by = c("UniqueClientID", "CcmhID", "Data_year"))
+  } else{
+
+    data <- data |>
+      dplyr::summarise(total.scheduled.appts = dplyr::n(),
+                       total.scheduled.ind.appts = length(which(appts_type == "Individual")),
+                       total.scheduled.group.appts = length(which(appts_type == "Group")),
+                       total.attended.appts = sum(Attended),
+                       total.attended.ind.appts = length(which(appts_type == "Individual" & Attended == 1)),
+                       total.attended.group.appts = length(which(appts_type == "Group" & Attended == 1)),
+                       .by = c("UniqueClientID", "CcmhID"))
+
+  }
+
+  # Recode NA to 0
+  if(recode_NA == TRUE){
+
+    data <- data |>
+      dplyr::mutate(dplyr::across(dplyr::starts_with("total"), ~ifelse(.x == 0, NA, .x)))
+
+  } else{
+
+  }
 
   # Return the cleaned data
   return(data)
--- R/summarize_clicc.r
+++ R/summarize_clicc.r
@@ -1,181 +1,219 @@
-#' Summarize CLICC data
+#' Summarize CLICC data.
 #'
-#' @description Summarize CLICC data
-#' @param data A data file containing clicc.
-#' @param clicc_type A vector containing the CLICC type. Options include: "checkall" or "topconcern". By default, `"checkall"`.
-#' @param group_vars A vector of grouping variables. By default, `NULL`.
-#' @param save A logical statement indicates whether the table should be saved as a file under a local folder. If false, the table will be returned as an object. By default, `FALSE`.
-#' @param path A quoted string to indicate the file's pathway and name if `save = TRUE`. By default, `"clicc_summary.csv"`. 
-#' 
-#' @return Prints demographic tables in concole and/or saves them in under the working directory.
+#' @description This function cleans and summarizes data from the CLICC. The function returns a dataset containing summarized CLICC data. The summarization process creates the following new variables: sample.n (The number of each CLICC concern being endorsed) and percent (The percentage that a CLICC item is endorsed). The clicc_type argument allows the researcher to specify whether the summary should be based on checking all that apply (CLICC_01\_##) or the top concern (CLICC_03). The summary could also be in relation to specific grouping variables using the group_vars argument.
+#'
+#' @param data A data frame containing CLICC.
+#' @param clicc_type A quoted string to indicate whether the summary should be based on checking all that apply (CLICC_01\_##) or the top concern (CLICC_03). Options include: `"checkall"` or `"topconcern"`. By default, `"checkall"`.
+#' @param group_vars A quoted string or list of quoted strings to specify if grouping variables should influence the summary. By default, `NULL`.
+#' @param save A logical argument that indicates whether the summarized data should be saved as a file under a local folder. If `FALSE`, the summarized data will be returned as an object. By default, `FALSE`.
+#' @param path A quoted string to indicate the file's type, pathway, and name if save = `TRUE`. By default, `"clicc_summary.csv"`.
+#'
+#' @return A data frame containing details on checking all that apply or top CLICC concerns. Variables returned in the dataset include variables in the CLICC key (Item, Concern, and Number), sample.n, and percent.
+#'
+#' @importFrom dplyr select
+#' @importFrom dplyr mutate
+#' @importFrom dplyr summarise
+#' @importFrom dplyr contains
+#' @importFrom dplyr n_distinct
+#' @importFrom dplyr first
+#' @importFrom dplyr arrange
+#' @importFrom dplyr inner_join
+#' @importFrom dplyr desc
+#' @importFrom dplyr group_by
+#' @importFrom dplyr all_of
+#' @importFrom dplyr ungroup
+#' @importFrom dplyr recode
+#' @importFrom dplyr case_when
+#' @importFrom dplyr across
+#' @importFrom dplyr rename
+#' @importFrom dplyr n
+#' @importFrom tidyr pivot_longer
+#' @importFrom tidyr drop_na
+#'
 #' @export
-#' 
-#' 
 
-summarize_clicc <- function(data, 
+summarize_clicc <- function(data,
                             clicc_type = "checkall",
                             group_vars = NULL,
-                            save = FALSE, 
-                            path = "clicc_summary.csv") {
+                            save = FALSE,
+                            path = "clicc_summary.csv"){
 
-    # Check if variables are in data
-    if(!all(c("UniqueClientID") %in% colnames(data))) {
-        stop("The data does not contain UniqueClientID.")
-    } else {
-       # Skip
-    }
+  # Check if variables are in data
+  if(!all(c("UniqueClientID") %in% colnames(data))){
 
-    # group_vars
-    if(!all(group_vars %in% colnames(data))){
-        stop("The data does not contain the grouping variables specified in group_vars.")
-    } else {
-       # Skip
-    }
+    stop("The data does not contain UniqueClientID.")
 
-    # check if clicc_type is "checkall" or "topconcern"
-    if(!clicc_type %in% c("checkall", "topconcern")){
-        stop("The clicc_type must be either 'checkall' or 'topconcern'.")
-    } else {
-       # Skip
-    }
-    
-    # Check all
-    if("checkall" == clicc_type){
-        
-        data$CLICC_01_1006[which(data$CLICC_01_06 == 1)] <- 1
-        data$CLICC_01_1028[which(data$CLICC_01_28 == 1)] <- 1
-        data$CLICC_01_06 <- NULL
-        data$CLICC_01_28 <- NULL
+  } else{
 
-        data <- mutate(data, 
-                       CLICC_01_01 = case_when(CLICC_01_1101 == 1 ~ 1,
-                                               CLICC_01_1102 == 1 ~ 1,
-                                               CLICC_01_1103 == 1 ~ 1,
-                                               CLICC_01_1104 == 1 ~ 1,
-                                               CLICC_01_1105 == 1 ~ 1,
-                                               CLICC_01_1106 == 1 ~ 1,
-                                               CLICC_01_01 == 1 ~ 1))
+  }
 
-        if(is.null(group_vars)){
-        
-        # Data filter
-        df.clicc.check.all <- data %>%
-            dplyr::select(UniqueClientID,
-                          dplyr::contains("CLICC_01")) %>%
-            tidyr::pivot_longer(c(CLICC_01_01:CLICC_01_1106), 
-                                names_to = "Item", 
-                                values_to = "value") %>%
-            tidyr::drop_na() %>%
-            dplyr::mutate(sample.n = dplyr::n_distinct(UniqueClientID)) %>%
-            dplyr::summarise(percent = sum(value == 1, na.rm = TRUE)/sample.n, 
-                             sample.n = dplyr::first(sample.n),
-                             .by = c("Item")) %>%
-            dplyr::arrange(dplyr::desc(percent)) %>%
-            dplyr::inner_join(CCMHr::clicc_key)
+  # Checking group_vars
+  if(!all(group_vars %in% colnames(data))){
 
-           df.clicc.check.all <- unique(df.clicc.check.all)
+    stop("The data does not contain the grouping variables specified in group_vars.")
 
-        } else {
-        
-        # Data filter
-        df.clicc.check.all <- data %>%
-            dplyr::select(UniqueClientID, group_vars, 
-                          dplyr::contains("CLICC_01")) %>%
-            tidyr::pivot_longer(c(CLICC_01_01:CLICC_01_1106), 
-                                names_to = "Item", 
-                                values_to = "value") %>%
-            tidyr::drop_na() %>%
-            dplyr::mutate(sample.n = dplyr::n_distinct(UniqueClientID), 
-                          .by = group_vars) %>%
-            dplyr::summarise(percent = sum(value == 1, na.rm = TRUE)/sample.n, 
-                             .by = c("Item", group_vars))  %>%
-            dplyr::group_by(dplyr::across(dplyr::all_of(group_vars))) %>%
-            dplyr::arrange(dplyr::desc(percent), .by_group = TRUE) %>%
-            dplyr::ungroup() %>%
-            dplyr::inner_join(CCMHr::clicc_key)
-            
-           df.clicc.check.all <- unique(df.clicc.check.all)
+  } else {
 
-        }
+  }
 
-        # Save file 
-        if(save == TRUE){
-            write.csv(df.clicc.check.all, 
-                      file = path, 
-                      row.names = FALSE)
-        } else {
-            return(df.clicc.check.all)
-        }
+  # Check if clicc_type is "checkall" or "topconcern"
+  if(!clicc_type %in% c("checkall", "topconcern")){
+
+    stop("The clicc_type must be either 'checkall' or 'topconcern'.")
+
+  } else {
+
+  }
+
+  # Check all
+  if("checkall" == clicc_type){
+
+    # Mutate data
+    data$CLICC_01_1006[which(data$CLICC_01_06 == 1)] <- 1
+    data$CLICC_01_1028[which(data$CLICC_01_28 == 1)] <- 1
+    data$CLICC_01_06 <- NULL
+    data$CLICC_01_28 <- NULL
+
+    data <- data |>
+      dplyr::mutate(CLICC_01_01 = dplyr::case_when(CLICC_01_1101 == 1 ~ 1,
+                                                   CLICC_01_1102 == 1 ~ 1,
+                                                   CLICC_01_1103 == 1 ~ 1,
+                                                   CLICC_01_1104 == 1 ~ 1,
+                                                   CLICC_01_1105 == 1 ~ 1,
+                                                   CLICC_01_1106 == 1 ~ 1,
+                                                   CLICC_01_01 == 1 ~ 1))
+
+    # Specify group_vars
+    if(is.null(group_vars)){
+
+      # Data filter
+      df.clicc.check.all <- data |>
+        dplyr::select(UniqueClientID,
+                      dplyr::contains("CLICC_01")) |>
+        tidyr::pivot_longer(c(CLICC_01_01:CLICC_01_1106),
+                            names_to = "Item",
+                            values_to = "value") |>
+        tidyr::drop_na() |>
+        dplyr::mutate(sample.n = dplyr::n_distinct(UniqueClientID)) |>
+        dplyr::summarise(percent = sum(value == 1, na.rm = TRUE)/sample.n,
+                         sample.n = dplyr::first(sample.n),
+                         .by = c("Item")) |>
+        dplyr::arrange(dplyr::desc(percent)) |>
+        dplyr::inner_join(CCMHr::clicc_key)
+
+      df.clicc.check.all <- unique(df.clicc.check.all)
 
     } else {
-       #Skip
+
+      # Data filter
+      df.clicc.check.all <- data |>
+        dplyr::select(UniqueClientID, group_vars,
+                      dplyr::contains("CLICC_01")) |>
+        tidyr::pivot_longer(c(CLICC_01_01:CLICC_01_1106),
+                            names_to = "Item",
+                            values_to = "value") |>
+        tidyr::drop_na() |>
+        dplyr::mutate(sample.n = dplyr::n_distinct(UniqueClientID),
+                      .by = group_vars) |>
+        dplyr::summarise(percent = sum(value == 1, na.rm = TRUE)/sample.n,
+                         .by = c("Item", group_vars))  |>
+        dplyr::group_by(dplyr::across(dplyr::all_of(group_vars))) |>
+        dplyr::arrange(dplyr::desc(percent), .by_group = TRUE) |>
+        dplyr::ungroup() |>
+        dplyr::inner_join(CCMHr::clicc_key)
+
+      df.clicc.check.all <- unique(df.clicc.check.all)
+
     }
 
-    # Top concern
-    if("topconcern" == clicc_type){
+    # Save file
+    if(save == TRUE){
 
-        data$CLICC_03 <- dplyr::recode(data$CLICC_03, 
-                                       `1101` = 1L, 
-                                       `1102` = 1L, 
-                                       `1103` = 1L, 
-                                       `1104` = 1L, 
-                                       `1105` = 1L, 
-                                       `1106` = 1L)
+      write.csv(df.clicc.check.all,
+                file = path,
+                row.names = FALSE)
 
-        data$CLICC_03[which(data$CLICC_03 == 6)] <- 1006
-        data$CLICC_03[which(data$CLICC_03 == 28)] <- 1028
+    } else{
 
-        if(is.null(group_vars)){
-        
-        # Data filter
-        df.clicc.check.top <- data %>%
-            dplyr::select(UniqueClientID,
-                          dplyr::contains("CLICC_03")) %>%
-            tidyr::drop_na() %>%
-            dplyr::mutate(sample.n = dplyr::n_distinct(UniqueClientID)) %>%
-            dplyr::rename("Number" = "CLICC_03") %>%
-            dplyr::summarise(percent = dplyr::n()/sample.n,  
-                             percent = format(percent, scientific = FALSE),
-                             .by = c("Number")) %>%
-            dplyr::arrange(dplyr::desc(percent)) %>%
-            dplyr::inner_join(CCMHr::clicc_key)
+      return(df.clicc.check.all)
 
-            df.clicc.check.top <- unique(df.clicc.check.top)
-
-        } else {
-        
-        # Data filter
-        df.clicc.check.top <- data %>%
-            dplyr::select(UniqueClientID, group_vars, 
-                          dplyr::contains("CLICC_03")) %>%
-            tidyr::drop_na()%>%
-            dplyr::mutate(sample.n = dplyr::n_distinct(UniqueClientID), 
-                          .by = group_vars) %>%
-            dplyr::rename("Number" = "CLICC_03") %>%
-            dplyr::summarise(percent = dplyr::n()/sample.n, 
-                             percent = format(percent, scientific = FALSE),
-                             .by = c("Number", group_vars)) %>%
-            dplyr::group_by(across(all_of(group_vars))) %>%
-            dplyr::arrange(dplyr::desc(percent), .by_group = TRUE) %>%
-            dplyr::ungroup() %>%
-            dplyr::inner_join(CCMHr::clicc_key)
-            
-            df.clicc.check.top <- unique(df.clicc.check.top)
-
-
-        }
-
-        # Save file 
-        if(save == TRUE){
-            write.csv(df.clicc.check.top, 
-                      file = path, 
-                      row.names = FALSE)
-        } else {
-            return(df.clicc.check.top)
-        }
-
-    } else {
-       #Skip
     }
+
+  } else {
+
+  }
+
+  # Top concern
+  if("topconcern" == clicc_type){
+
+    # Data processing
+    data$CLICC_03 <- data |>
+      dplyr::recode(CLICC_03,
+                    `1101` = 1L,
+                    `1102` = 1L,
+                    `1103` = 1L,
+                    `1104` = 1L,
+                    `1105` = 1L,
+                    `1106` = 1L)
+
+    data$CLICC_03[which(data$CLICC_03 == 6)] <- 1006
+    data$CLICC_03[which(data$CLICC_03 == 28)] <- 1028
+
+    # Specify group_vars
+    if(is.null(group_vars)){
+
+      # Data filter
+      df.clicc.check.top <- data |>
+        dplyr::select(UniqueClientID,
+                      dplyr::contains("CLICC_03")) |>
+        tidyr::drop_na() |>
+        dplyr::mutate(sample.n = dplyr::n_distinct(UniqueClientID)) |>
+        dplyr::rename("Number" = "CLICC_03") |>
+        dplyr::summarise(percent = dplyr::n()/sample.n,
+                         percent = format(percent, scientific = FALSE),
+                         .by = c("Number")) |>
+        dplyr::arrange(dplyr::desc(percent)) |>
+        dplyr::inner_join(CCMHr::clicc_key)
+
+      df.clicc.check.top <- unique(df.clicc.check.top)
+
+    } else{
+
+      # Data filter
+      df.clicc.check.top <- data |>
+        dplyr::select(UniqueClientID, group_vars,
+                      dplyr::contains("CLICC_03")) |>
+        tidyr::drop_na()|>
+        dplyr::mutate(sample.n = dplyr::n_distinct(UniqueClientID),
+                      .by = group_vars) |>
+        dplyr::rename("Number" = "CLICC_03") |>
+        dplyr::summarise(percent = dplyr::n()/sample.n,
+                         percent = format(percent, scientific = FALSE),
+                         .by = c("Number", group_vars)) |>
+        dplyr::group_by(across(all_of(group_vars))) |>
+        dplyr::arrange(dplyr::desc(percent), .by_group = TRUE) |>
+        dplyr::ungroup() |>
+        dplyr::inner_join(CCMHr::clicc_key)
+
+      df.clicc.check.top <- unique(df.clicc.check.top)
+
+    }
+
+    # Save file
+    if(save == TRUE){
+
+      write.csv(df.clicc.check.top,
+                file = path,
+                row.names = FALSE)
+
+    } else{
+
+      return(df.clicc.check.top)
+
+    }
+
+  } else {
+
+  }
 
 }
