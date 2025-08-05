d522d14c841debec487ea2a32e96fbea322ef132
Tyler Dallas White <tylerwhite0120@gmail.com>
2025-08-05 09:06:56 -0400

    Updated documentation, standardized spacing/tabs, and added code notes when applicable. Also fixed a critical error that made it impossible to extract more than a single mode.

--- R/get_mode.R
+++ R/get_mode.R
@@ -1,13 +1,26 @@
-#' Get the mode of a vector
+#' Extract the mode value from a vector.
 #'
-#' @param x a vector
+#' @description The function extracts the mode value(s) from a vector. `NA` responses are omitted.
 #'
-#' @return Mode of x
+#' @param x A vector.
+#'
+#' @return The mode value(s) within a vector.
+#'
 #' @export
 
-
 get_mode <- function(x) {
-  values <- unique(stats::na.omit(x))
-  values[which.max(tabulate(match(x, values)))]
+
+  # Removing NA values from x
+  x <- x[!is.na(x)]
+
+  # Getting unique values from x
+  u <- unique(x)
+
+  # Tabulating the number of times each unique value appears in x
+  tab <- tabulate(match(x, u))
+
+  # Returning the value(s) that appear most frequently
+  u[tab == max(tab)]
+
 }
 
